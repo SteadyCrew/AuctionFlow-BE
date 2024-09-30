dabb4035a6f82a81d6f150b6457784d87290fc8c
hyunji <phj9573@naver.com>
2024-09-30 11:48:04 +0900

    first commit

---
+++ src/main/java/org/example/auctionflowbe/AuctionFlowBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.auctionflowbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionFlowBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuctionFlowBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/auctionflowbe/AuctionFlowBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.auctionflowbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionFlowBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
